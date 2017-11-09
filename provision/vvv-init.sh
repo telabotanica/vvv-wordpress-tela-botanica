a0a319bad071fd214f8642d162f37ce183d65df0
kstefanini <stefanini.k@gmail.com>
2017-11-09 12:21:30 +0100

    initial commit

---
+++ provision/vvv-init.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Provision WordPress Stable
+
+# Make a database, if we don't already have one
+echo -e "\nCreating database 'wordpress_tb' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS wordpress_tb"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON wordpress_tb.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\n DB operations done.\n\n"
+
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+# Install and configure the latest stable version of WordPress
+if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
+
+  echo "Downloading WordPress Stable, see http://wordpress.org/"
+  cd ${VVV_PATH_TO_SITE}
+  curl -L -O "https://wordpress.org/latest.tar.gz"
+  noroot tar -xvf latest.tar.gz
+  mv wordpress public_html
+  rm latest.tar.gz
+  cd ${VVV_PATH_TO_SITE}/public_html
+
+  echo "Configuring WordPress Stable..."
+  noroot wp core config --dbname=wordpress_tb --dbuser=wp --dbpass=wp --quiet --extra-php <<PHP
+// Match any requests made via xip.io.
+if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(local.tela-botanica.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
+    define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
+    define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
+}
+
+define( 'WP_DEBUG', true );
+PHP
+
+  echo "Installing WordPress Stable..."
+  noroot wp core install --url=local.tela-botanica.dev --quiet --title="Local WordPress Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"
+
+else
+
+  echo "Updating WordPress Stable..."
+  cd ${VVV_PATH_TO_SITE}/public_html
+  noroot wp core update
+
+fi
